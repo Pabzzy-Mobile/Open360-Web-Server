8130d90ea0ddca43924879e223e25d14cdc351ce
Pedro Pires <plpires99@gmail.com>
2020-10-20 16:47:32 +0100

    Added some comments to the test function

--- core/tests.js
+++ core/tests.js
@@ -57,26 +57,32 @@ Tests.DatabaseTests.testInsertDelete = function (){
     let insertPassed = true;
     let deletePassed = true;
     let userToTest = new Util.UserData();
+    // Set the user's details
     userToTest.type = Util.UserDataTypes.JUST_AUTH
     userToTest.userId = "c9dc993100e7fc30dbc0769deb19dba15bfbc5bf";
     userToTest.username = "Jonny554";
     userToTest.password = "f77d7271e109fc01ea3bee60052b9e671886cadebec3a6aea3f1a2b1c42014b8"
     userToTest.salt = "6RtAMLCs";
+    // Add the user to the database
     DatabaseAccess.write.addUserAuth(userToTest, function (err){
         if (err) console.error(err);
     });
+    // Find the user and check if it was successfully added
     DatabaseAccess.find.userAuth("c9dc993100e7fc30dbc0769deb19dba15bfbc5bf", function (user){
         // Check if the user can be found
         insertPassed = user.userId === userToTest.userId;
         insertPassed &= user.username === userToTest.username;
     });
+    // Remove the user from the database
     DatabaseAccess.getCollection("user_auth", function (client, usersCollection){
         usersCollection.deleteOne({userId: "c9dc993100e7fc30dbc0769deb19dba15bfbc5bf", username: "jonny554"});
     });
+    // Check if the user is still on the database
     DatabaseAccess.find.userAuth("c9dc993100e7fc30dbc0769deb19dba15bfbc5bf", function (user){
         // Check if the user can be found
         deletePassed = user === null
     });
+    // Check if the tests passed and send some error info if not
     if (!insertPassed) console.error("Database User insertion didn't pass");
     if (!deletePassed) console.error("Database User deletion didn't pass");
     return insertPassed && deletePassed;
---
+++ networks and hosts.md
@@ -0,0 +1,7 @@
+# Redis Service
+- Network : open360-web-server_session-redis-network
+- Hostname: open360-redis-session
+
+# Mongo Service
+- Network : open360-web-server_mongodb-network
+- Hostname: open360-mongodb
